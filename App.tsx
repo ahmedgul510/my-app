c5084f31dd44df8c845faf5d9680408473a80249
ahmedgul510 <162698530+ahmedgul510@users.noreply.github.com>
2024-03-17 03:02:16 +0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { SignupForm } from './components/Form/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <SignupForm/>
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
